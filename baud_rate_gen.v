5c1893ad579d196ce49755923a2aeb32ba471c62
Saqib Daniel <saqib13062003@gmail.com>
2026-09-20 02:44:42 -0300

    Add rx fsm

--- baud_rate_gen.v
+++ baud_rate_gen.v
@@ -1,9 +1,9 @@
 `timescale 1ns / 1ps
 
 module baud_rate_gen #(
-  parameter CLK_FREQ = 50_000_000,
+  parameter CLK_FREQ = 50000000,
   parameter OVERSAMPLING = 16,
-  parameter BAUD_RATE = 19_200
+  parameter BAUD_RATE = 19200
 )(
   input wire clk,
   input wire reset,
@@ -11,10 +11,10 @@ module baud_rate_gen #(
 );
 
   // Cantidad de ciclos de clock entre ticks
-  localparam CICLOS_PER_TICK =
-      (CLK_FREQ + (BAUD_RATE * OVERSAMPLING)/2)
-      / (BAUD_RATE * OVERSAMPLING);
-
+  // localparam CICLOS_PER_TICK = (CLK_FREQ + (BAUD_RATE * OVERSAMPLING)/2) / (BAUD_RATE * OVERSAMPLING);
+  // localparam CICLOS_PER_TICK = (CLK_FREQ) / (BAUD_RATE * OVERSAMPLING);
+  localparam CICLOS_PER_TICK = 163;  
+  
   // Cantidad de bits necesarios para el contador
   localparam COUNTER_WIDTH = $clog2(CICLOS_PER_TICK);
 
@@ -26,7 +26,6 @@ module baud_rate_gen #(
       counter <= 0;
     end
     else if (counter == CICLOS_PER_TICK - 1) begin
-      tick <= (counter == CICLOS_PER_TICK - 1)
       counter <= 0;
     end
     else begin
